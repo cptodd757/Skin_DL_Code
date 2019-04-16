dfef0395dc37e32176bb1c5b156cec4fc657fbe1
cptodd757 <36899320+cptodd757@users.noreply.github.com>
2019-04-16 17:12:18 -0400

    Update load.py

--- load.py
+++ load.py
@@ -15,4 +15,32 @@ from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 from keras.layers import Input
 
-model = keras.models.load_model('MobileNet.h5')
+datagen = ImageDataGenerator(preprocessing_function = keras.applications.mobilenet.preprocess_input,
+                                    rotation_range = 180,
+                                    width_shift_range = 0.1,
+                                    height_shift_range = 0.1,
+                                    zoom_range = 0.1,
+                                    horizontal_flip = True,
+                                    vertical_flip = True,
+                                    #brightness_range = (0.9,1.1),
+                                    fill_mode = 'nearest')
+
+test_batches = datagen.flow_from_directory('test_dir', target_size = (64, 64), batch_size  = 1, shuffle=False)
+
+
+
+def top_3_accuracy(y_true, y_pred):
+  return top_k_categorical_accuracy(y_true, y_pred, k=3)
+def top_2_accuracy(y_true, y_pred):
+  return top_k_categorical_accuracy(y_true, y_pred, k=2)
+
+
+
+
+
+model = keras.models.load_model("MobileNet.h5")
+val_loss, val_cat_acc, val_top_2_acc, val_top_3_acc = model.evaluate_generator(test_batches, steps=len(os.listdir('test_dir')))
+print('val_loss:', val_loss)
+print('val_cat_acc:', val_cat_acc)
+print('val_top_2_acc:', val_top_2_acc)
+print('val_top_3_acc:', val_top_3_acc)
